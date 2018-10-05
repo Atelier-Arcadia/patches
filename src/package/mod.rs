b8908c799283ad9dd6dd20e9f9f4fdbe53989b88
Zack Mullaly <zmullaly@mozilla.com>
2018-10-05 14:53:38 -0400

    Cleaning up our module interface a bit and defining a trait for detecting packages installed on a host

--- src/package/mod.rs
+++ src/package/mod.rs
@@ -1,79 +1,12 @@
-use std::cmp::{PartialOrd, Ordering};
+mod package;
 
-use semver::{SemVerError, Version};
-
-
-// Contains information describing a package installed on a host.
-#[derive(Clone, Debug)]
-pub struct Package {
-  pub name: String,
-  pub version: Version,
-}
-
-impl Package {
-  // Construct a package with a name and specific version.
-  pub fn new<S: Into<String>>(name: S, ver: Version) -> Self {
-    Package {
-      name: name.into(),
-      version: ver,
-    }
-  }
-
-  // Attempt to construct a package with a name and a version.
-  // Here, the version is given as a string which is parsed with the assumption that
-  // it describes a semantic version.
-  pub fn with_version_string<S1, S2>(name: S1, ver: S2) -> Result<Package, SemVerError>
-    where S1: Into<String>,
-          S2: AsRef<str>,
-  {
-    Version::parse(ver.as_ref())
-      .map(|version| Package {
-        name: name.into(),
-        version: version,
-      })
-  }
-}
-
-impl PartialEq for Package {
-  fn eq(&self, other: &Package) -> bool {
-    self.name == other.name && self.version == other.version
-  }
-}
-
-impl Eq for Package{}
-
-impl PartialOrd for Package {
-  fn partial_cmp(&self, other: &Package) -> Option<Ordering> {
-    if self.name != other.name {
-      None
-    } else {
-      self.version.partial_cmp(&other.version)
-    }
-  }
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  use std::cmp::{PartialOrd, Ordering};
+pub use package::Package;
 
 
-  #[test]
-  fn package_comparison() {
-    let p1 = Package::with_version_string("mig", "3.14.15").unwrap();
-    let p2 = Package::with_version_string("mig", "2.1.8").unwrap();
-    let p3 = Package::with_version_string("mozdef", "10.42.1").unwrap();
-    let p4 = Package::with_version_string("mig", "3.14.15").unwrap();
-    let p5 = Package::with_version_string("mozdef", "3.14.15").unwrap();
+// An interface for different means of searching for a package on a host.
+pub trait Detect {
+  type Error;
 
-    assert!(p1 != p2);
-    assert!(p2 != p3);
-    assert!(p3 != p4);
-
-    assert!(p1 == p4);
-
-    assert!(p1.partial_cmp(&p2).unwrap() == Ordering::Greater);
-    assert!(p1.partial_cmp(&p5).is_none());
-  }
+  // Determine whether a given package is installed.
+  fn detect(&self, &Package) -> Result<bool, Self::Error>;
 }
