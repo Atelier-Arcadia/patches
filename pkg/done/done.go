b84fc2c48d02530b6ab7fddedb201a766795b6f4
Zack Mullaly <zsck@riseup.net>
2018-11-01 21:26:26 -0400

    Laid out some foundational types and interfaces for the core of patches' funcionality

---
+++ pkg/done/done.go
@@ -0,0 +1,5 @@
+package done
+
+// Done is a flag that should be sent through a channel by types that stream
+// data via a channel.
+type Done struct{}
---
+++ pkg/pack/pack.go
@@ -0,0 +1,17 @@
+package pack
+
+// Scanner describes types that support the behaviour of scanning the host for
+// an installation of a given package.
+type Scanner interface {
+	Scan(Package) (Found, error)
+}
+
+// Package describes a software package that may be installed on a host.
+type Package struct {
+	Name    string `json:"name"`
+	Version string `json:"name"`
+}
+
+// Found is a descriptive alias for a boolean produced by a Scanner that has
+// performed a scan for a particular package.
+type Found bool
