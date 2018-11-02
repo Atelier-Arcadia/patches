28f6cfc3d7e5f7d40c9d47d46ffb6f3e10445b97
Zack Mullaly <zmullaly@mozilla.com>
2018-11-02 16:59:59 -0400

    Cleaned up the API code a bit

--- internal/sources/clair/clair.go
+++ internal/sources/clair/clair.go
@@ -67,105 +67,118 @@ func summarizeVulnerabilities(
 	finished := make(chan done.Done)
 	errs := make(chan error)
 
-	// Collect makes requests to the Clair API until it has enumerated all of the
-	// pages containing names of vulnerabilities for the platform requested.
-	__collect := func() {
-		base, err := url.Parse(config.BaseURL)
-		if err != nil {
-			errs <- err
-			return
+	go __collect(config, platform, summaries, finished, errs)
+	return summaries, finished, errs
+}
+
+// __collect makes requests to the Clair API until it has enumerated all of the
+// pages containing names of vulnerabilities for the platform requested.
+func __collect(
+	cfg ClairAPIv1,
+	platform Platform,
+	summaries chan<- summary,
+	finished chan<- done.Done,
+	errs chan<- error,
+) {
+	base, err := url.Parse(cfg.BaseURL)
+	if err != nil {
+		errs <- err
+		return
+	}
+
+	ext := fmt.Sprintf(vulnSummariesWithoutPageEndptFmt, platform)
+	nextPage := __get(base, ext, summaries, errs)
+	for nextPage != "" {
+		ext = fmt.Sprintf(vulnSummariesWithPageEndptFmt, platform, nextPage)
+		nextPage = __get(base, ext, summaries, errs)
+	}
+	finished <- done.Done{}
+}
+
+// __get fetches a page of vulnerability summaries and returns the next
+// page's identifier, or an empty string if there is not one.
+func __get(
+	base *url.URL,
+	ext string,
+	summaries chan<- summary,
+	errs chan<- error,
+) string {
+	endpt, _ := url.Parse(ext)
+	toReq := base.ResolveReference(endpt)
+
+	response, err := http.Get(toReq.String())
+	if err != nil {
+		errs <- err
+		return ""
+	}
+	defer response.Body.Close()
+
+	respJSON := map[string]interface{}{}
+	decoder := json.NewDecoder(response.Body)
+	decodeErr := decoder.Decode(&respJSON)
+	if decodeErr != nil {
+		errs <- decodeErr
+		return ""
+	}
+
+	if errMsg, convertErr := __toErrorResponse(respJSON); convertErr == nil {
+		errs <- errors.New(errMsg.Error.Message)
+		return ""
+	}
+	if success, convertErr := __toSummaryResponse(respJSON); convertErr == nil {
+		for _, vuln := range success.Vulnerabilities {
+			summaries <- vuln
 		}
 
-		__toErrorResponse := func(jsonData map[string]interface{}) (errorResponse, error) {
-			theError := errors.New("Not an error response")
+		if success.NextPage != nil {
+			return *success.NextPage
+		}
+		return ""
+	}
+	return ""
+}
 
-			if err, ok := jsonData["Error"].(map[string]interface{}); ok {
-				if msg, ok := err["Message"].(string); ok {
-					errResp := errorResponse{
-						Error: message{
-							Message: msg,
-						},
-					}
-					return errResp, nil
-				} else {
-					return errorResponse{}, theError
-				}
+func __toErrorResponse(jsonData map[string]interface{}) (errorResponse, error) {
+	theError := errors.New("Not an error response")
+
+	if err, ok := jsonData["Error"].(map[string]interface{}); ok {
+		if msg, ok := err["Message"].(string); ok {
+			errResp := errorResponse{
+				Error: message{
+					Message: msg,
+				},
 			}
+			return errResp, nil
+		} else {
 			return errorResponse{}, theError
 		}
+	}
+	return errorResponse{}, theError
+}
 
-		__toSummaryResponse := func(jsonData map[string]interface{}) (summaryResponse, error) {
-			sumResp := summaryResponse{}
-			theError := errors.New("Not a summary response")
+func __toSummaryResponse(jsonData map[string]interface{}) (summaryResponse, error) {
+	sumResp := summaryResponse{}
+	theError := errors.New("Not a summary response")
 
-			if nextPage, ok := jsonData["NextPage"].(string); ok {
-				sumResp.NextPage = new(string)
-				*sumResp.NextPage = nextPage
-			}
+	if nextPage, ok := jsonData["NextPage"].(string); ok {
+		sumResp.NextPage = new(string)
+		*sumResp.NextPage = nextPage
+	}
 
-			if vulnBlobs, ok := jsonData["Vulnerabilities"].([]interface{}); ok {
-				for _, vulnBlob := range vulnBlobs {
-					vulnJSON, _ := vulnBlob.(map[string]interface{})
-					if name, ok := vulnJSON["Name"].(string); ok {
-						sumResp.Vulnerabilities = append(sumResp.Vulnerabilities, summary{name})
-					} else {
-						return summaryResponse{}, theError
-					}
-				}
+	if vulnBlobs, ok := jsonData["Vulnerabilities"].([]interface{}); ok {
+		for _, vulnBlob := range vulnBlobs {
+			vulnJSON, _ := vulnBlob.(map[string]interface{})
+			if name, ok := vulnJSON["Name"].(string); ok {
+				sumResp.Vulnerabilities = append(sumResp.Vulnerabilities, summary{name})
 			} else {
 				return summaryResponse{}, theError
 			}
-
-			return sumResp, nil
 		}
-
-		// __get fetches a page of vulnerability summaries and returns the next
-		// page's identifier, or an empty string if there is not one.
-		__get := func(ext string) string {
-			endpt, _ := url.Parse(ext)
-			toReq := base.ResolveReference(endpt)
-
-			response, err := http.Get(toReq.String())
-			if err != nil {
-				errs <- err
-				return ""
-			}
-			defer response.Body.Close()
-
-			respJSON := map[string]interface{}{}
-			decoder := json.NewDecoder(response.Body)
-			decodeErr := decoder.Decode(&respJSON)
-			if decodeErr != nil {
-				errs <- decodeErr
-				return ""
-			}
-
-			if errMsg, convertErr := __toErrorResponse(respJSON); convertErr == nil {
-				errs <- errors.New(errMsg.Error.Message)
-				return ""
-			}
-			if success, convertErr := __toSummaryResponse(respJSON); convertErr == nil {
-				for _, vuln := range success.Vulnerabilities {
-					summaries <- vuln
-				}
-
-				if success.NextPage != nil {
-					return *success.NextPage
-				}
-				return ""
-			}
-			return ""
-		}
-
-		nextPage := __get(fmt.Sprintf(vulnSummariesWithoutPageEndptFmt, platform))
-		for nextPage != "" {
-			nextPage = __get(fmt.Sprintf(vulnSummariesWithPageEndptFmt, platform, nextPage))
-		}
-		finished <- done.Done{}
+	} else {
+		return summaryResponse{}, theError
 	}
 
-	go __collect()
-	return summaries, finished, errs
+	return sumResp, nil
 }
 
 func (p Platform) String() string {
