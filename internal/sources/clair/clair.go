bdecaf811dd357845fe4ab833080b172aeb5ab1d
Zack Mullaly <zmullaly@mozilla.com>
2018-11-02 19:19:40 -0400

    First implementation of the describeVulnerability function

--- internal/sources/clair/clair.go
+++ internal/sources/clair/clair.go
@@ -12,6 +12,7 @@ import (
 
 const vulnSummariesWithoutPageEndptFmt string = "/v1/namespaces/%s/vulnerabilities?limit=100"
 const vulnSummariesWithPageEndptFmt string = "/v1/namespaces/%s/vulnerabilities?page=%s&limit=100"
+const vulnDescriptionEndptFmt string = "/v1/namespaces/%s/vulnerabilities/%s"
 
 // ClairAPIv1 contains the configuration required to communicate with V1 of the Clair API.
 type ClairAPIv1 struct {
@@ -34,6 +35,22 @@ var (
 
 type summary struct {
 	Name string `json:"Name"`
+}
+
+type fix struct {
+	Name    string `json:"Name"`
+	Version string `json:"Version"`
+}
+
+type description struct {
+	Name     string `json:"Name"`
+	Link     string `json:"Link"`
+	Severity string `json:"Severity"`
+	FixedIn  []fix  `json:"FixedIn"`
+}
+
+type descriptionResponse struct {
+	Vulnerability description `json:"Vulnerability"`
 }
 
 type summaryResponse struct {
@@ -71,6 +88,21 @@ func summarizeVulnerabilities(
 	return summaries, finished, errs
 }
 
+// describeVulnerability runs a goroutine that requests detailed infomration
+// about a vulnerability affecting a platform from the Clair v1 API.
+func describeVulnerability(
+	config ClairAPIv1,
+	vulnName string,
+	platform Platform,
+) (<-chan description, <-chan done.Done, <-chan error) {
+	descriptions := make(chan description)
+	finished := make(chan done.Done)
+	errs := make(chan error)
+
+	go __describe(config, vulnName, platform, descriptions, finished, errs)
+	return descriptions, finished, errs
+}
+
 // __collect makes requests to the Clair API until it has enumerated all of the
 // pages containing names of vulnerabilities for the platform requested.
 func __collect(
@@ -91,6 +123,51 @@ func __collect(
 	for nextPage != "" {
 		ext = fmt.Sprintf(vulnSummariesWithPageEndptFmt, platform, nextPage)
 		nextPage = __getSummaries(base, ext, summaries, errs)
+	}
+	finished <- done.Done{}
+}
+
+// __describe performs a request to the Clair API to get detailed information
+// about one specific vulnerability.
+func __describe(
+	cfg ClairAPIv1,
+	vulnName string,
+	platform Platform,
+	descriptions chan<- description,
+	finished chan<- done.Done,
+	errs chan<- error,
+) {
+	base, err := url.Parse(cfg.BaseURL)
+	if err != nil {
+		errs <- err
+		return
+	}
+
+	ext := fmt.Sprintf(vulnDescriptionEndptFmt, platform, vulnName)
+	endpt, _ := url.Parse(ext)
+	toReq := base.ResolveReference(endpt)
+
+	response, err := http.Get(toReq.String())
+	if err != nil {
+		errs <- err
+		return
+	}
+	defer response.Body.Close()
+
+	respJSON := map[string]interface{}{}
+	decoder := json.NewDecoder(response.Body)
+	decodeErr := decoder.Decode(&respJSON)
+	if decodeErr != nil {
+		errs <- decodeErr
+		return
+	}
+
+	if errMsg, convertErr := __toErrorResponse(respJSON); convertErr == nil {
+		errs <- errors.New(errMsg.Error.Message)
+		return
+	}
+	if description, convertErr := __toDescriptionResponse(respJSON); convertErr == nil {
+		descriptions <- description
 	}
 	finished <- done.Done{}
 }
@@ -179,6 +256,54 @@ func __toSummaryResponse(jsonData map[string]interface{}) (summaryResponse, erro
 	}
 
 	return sumResp, nil
+}
+
+func __toDescriptionResponse(jsonData map[string]interface{}) (descriptionResponse, error) {
+	desc := descriptionResponse{}
+	theError := errors.New("Not a vulnerability description")
+
+	if name, ok := jsonData["Name"].(string); ok {
+		desc.Vulnerability.Name = name
+	} else {
+		return descriptionResponse{}, theError
+	}
+
+	if link, ok := jsonData["Link"].(string); ok {
+		desc.Vulnerability.Link = link
+	} else {
+		return descriptionResponse{}, theError
+	}
+
+	if severity, ok := jsonData["Severity"].(string); ok {
+		desc.Vulnerability.Severity = severity
+	} else {
+		return descriptionResponse{}, theError
+	}
+
+	if fixBlobs, ok := jsonData["FixedIn"].([]interface{}); ok {
+		for _, blob := range fixBlobs {
+			newFix := fix{}
+			fixJSON, _ := blob.(map[string]interface{})
+
+			if name, ok := fixJSON["Name"].(string); ok {
+				newFix.Name = name
+			} else {
+				return descriptionResponse{}, theError
+			}
+
+			if version, ok := fixJSON["Version"].(string); ok {
+				newFix.Version = version
+			} else {
+				return descriptionResponse{}, theError
+			}
+
+			desc.Vulnerability.FixedIn = append(desc.Vulnerability.FixedIn, newFix)
+		}
+	} else {
+		return descriptionResponse{}, theError
+	}
+
+	return desc, nil
 }
 
 func (p Platform) String() string {
