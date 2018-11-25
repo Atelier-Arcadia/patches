03cb7329e7eb4321b476870c8347176635b03fb5
Zack Mullaly <emma.arcadia.rose@gmail.com>
2018-11-24 22:58:27 -0500

    First stab at implementing a client

--- internal/clients/clients.go
+++ internal/clients/clients.go
@@ -1,22 +1,40 @@
 package clients
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/arcrose/patches/pkg/done"
 	"github.com/arcrose/patches/pkg/platform"
 	"github.com/arcrose/patches/pkg/vulnerability"
+
+	"github.com/arcrose/patches/internal/limit"
 )
+
+const startFetchEndptFmt string = "%s:%s/vulns?platform=%s"
+const continueFetchEndptFmt string = "%s:%s/vulns?platform=%s&requestID=%s"
 
 // ClairClient is capable of communicating with a vulnerability source over HTTP.
 type ClairClient struct {
 	serverAddr string
 	serverPort uint16
+	block      limit.RateLimiter
+}
+
+type clairServerResponse struct {
+	Error           *string                       `json:"error"`
+	RequestID       string                        `json:"requestID"`
+	Finished        bool                          `json:"finished"`
+	Vulnerabilities []vulnerability.Vulnerability `json:"vulns"`
 }
 
 // NewClairClient constructs a ClairClient.
-func NewClairClient(addr string, port uint16) ClairClient {
+func NewClairClient(addr string, port uint16, limiter limit.RateLimiter) ClairClient {
 	return ClairClient{
 		serverAddr: addr,
 		serverPort: port,
+		block:      limiter,
 	}
 }
 
@@ -42,5 +60,57 @@ func __retrieve(
 	finished chan<- done.Done,
 	errs chan<- error,
 ) {
+	requestID := ""
+
+	for {
+		url := ""
+		if requestID == "" {
+			url = fmt.Sprintf(
+				startFetchEndptFmt,
+				client.serverAddr,
+				client.serverPort,
+				pform.String())
+		} else {
+			url = fmt.Sprintf(
+				continueFetchEndptFmt,
+				client.serverAddr,
+				client.serverPort,
+				pform.String(),
+				requestID)
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			errs <- err
+			break
+		}
+
+		respData := clairServerResponse{}
+		decoder := json.NewDecoder(resp.Body)
+		decodeErr := decoder.Decode(&respData)
+		resp.Body.Close()
+		if decodeErr != nil {
+			errs <- decodeErr
+			break
+		}
+
+		if respData.Error != nil {
+			errs <- fmt.Errorf("%s", *respData.Error)
+			break
+		}
+
+		for _, vuln := range respData.Vulnerabilities {
+			vulns <- vuln
+		}
+
+		if respData.Finished {
+			break
+		}
+
+		requestID = respData.RequestID
+
+		<-client.block()
+	}
+
 	finished <- done.Done{}
 }
