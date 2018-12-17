6074b33449350de55fea08c6636e296b683f08bd
Emma Rose <emrose@mozilla.com>
2018-12-17 16:42:18 -0500

    Log vulnerabilities retrieved by the mock reportapi

--- reportapi.go
+++ reportapi.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -10,5 +12,8 @@ func main() {
 }
 
 func serveOK(res http.ResponseWriter, req *http.Request) {
+	body, _ := ioutil.ReadAll(req.Body)
+	req.Body.Close()
+	fmt.Printf("Got a vuln: %s\n", body)
 	res.Write([]byte("Ok"))
 }
